eaf73c3a3c07dc8eed2dd8c37fcf977a4d6035e6
Shaheen Salma <shaheencodex@gmail.com>
2025-01-13 23:48:08 +0530

    formCorrection

--- js/script.js
+++ js/script.js
@@ -15,22 +15,7 @@ function typeText(text, typingElement, delay) {
     }
 }
 
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const jsonData = Object.fromEntries(formData.entries());
-    
-    fetch('/submit', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(jsonData),
-    }).then(response => response.json()).then(data => {
-      console.log('Success:', data);
-    }).catch(error => {
-      console.error('Error:', error);
-    });
-  });
+
   
 document.addEventListener('DOMContentLoaded', runTypingEffect);
+
